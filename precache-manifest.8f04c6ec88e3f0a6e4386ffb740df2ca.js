8dbc461990d593dd6f158bb972958be285f38675
Sameed ahmad <53148681+Sameedahmad@users.noreply.github.com>
2019-12-27 18:46:48 +0500

    Updates

---
+++ precache-manifest.8f04c6ec88e3f0a6e4386ffb740df2ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b504f7c2deaa5e9fa3d775e1a1dd2164",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "fef08027e8fc460134d0",
+    "url": "/Robo-Friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "49d949154c83232e36b4",
+    "url": "/Robo-Friends/static/css/main.35992123.chunk.css"
+  },
+  {
+    "revision": "fef08027e8fc460134d0",
+    "url": "/Robo-Friends/static/js/2.aeb16598.chunk.js"
+  },
+  {
+    "revision": "49d949154c83232e36b4",
+    "url": "/Robo-Friends/static/js/main.7e53eb13.chunk.js"
+  },
+  {
+    "revision": "d5266c380c7141b024db",
+    "url": "/Robo-Friends/static/js/runtime~main.3277a611.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
